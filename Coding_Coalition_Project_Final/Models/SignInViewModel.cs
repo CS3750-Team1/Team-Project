c02b3dea08d1f1b7db4cb6d80d154037d2f88e6e
Bushmansmash <matt.bushman@yahoo.com>
2020-06-06 12:01:53 -0600

    Signin add
    Adding Signing fucntionality

---
+++ Coding_Coalition_Project_Final/Models/SignInViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coding_Coalition_Project.Models
+{
+    public class SignInViewModel
+    {
+
+        public int ID { get; set; }
+
+        [Required, RegularExpression(@"^['A-Za-z-@._'-]*")]
+        public string Email { get; set; }
+
+        [Required, RegularExpression(@"^['A-Za-z0-9""'\s-]*$"), DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
--- Coding_Coalition_Project_Final/Pages/SignIn/signin.cshtml.cs
+++ Coding_Coalition_Project_Final/Pages/SignIn/signin.cshtml.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Coding_Coalition_Project.Models;
-
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Identity.Core;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Coding_Coalition_Project.Pages.SignIn
 {
     public class signinModel : PageModel
     {
-        public void OnGet()
+        private readonly Coding_Coalition_Project.Data.Coding_Coalition_ProjectContext _context;
+
+        public signinModel(Coding_Coalition_Project.Data.Coding_Coalition_ProjectContext context)
         {
+            _context = context;
+        }
+
+        //public void OnGet()
+        public IActionResult OnGet()
+        {
+            return Page();
 
         }
 
-        public UserInfo UserInfo { get; set; }
+        [BindProperty]
+        public SignInViewModel SignInViewModel { get; set; }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Signin(UserInfo model)
+        {
+            if (!ModelState.IsValid)
+            {
+                var user = new ApplicationUser() { UserName = model.Email };
+                var result = await _context.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                }
+
+                ModelState.AddModelError(string.Empty, "Invalid Login Attempt");
+            }
+
+            return View(model);
+        }
+
+
     }
 }
