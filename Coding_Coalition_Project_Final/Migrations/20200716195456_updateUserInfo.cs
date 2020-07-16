ff5a507643270475c867958eb705d8e90d635fda
Bushmansmash <matt.bushman@yahoo.com>
2020-07-16 13:59:07 -0600

    updateUserInfo Migration

---
+++ Coding_Coalition_Project_Final/Migrations/20200716195456_updateUserInfo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Coding_Coalition_Project.Migrations
+{
+    public partial class updateUserInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+               name: "CreditHours",
+               table: "UserInfo",
+               nullable: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountCharges",
+                table: "UserInfo",
+                nullable: false);
+
+            migrationBuilder.AddColumn<int>(
+               name: "AccountPayments",
+               table: "UserInfo",
+               nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
