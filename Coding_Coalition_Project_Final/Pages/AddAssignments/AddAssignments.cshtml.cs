0849c7c379db206426f7c4b9a9ec3b9690a4c3c2
keltonjohnson004 <52982698+keltonjohnson004@users.noreply.github.com>
2020-07-18 12:00:15 -0600

    Fixed adding assignments

--- Coding_Coalition_Project_Final/Pages/AddAssignments/AddAssignments.cshtml.cs
+++ Coding_Coalition_Project_Final/Pages/AddAssignments/AddAssignments.cshtml.cs
@@ -27,7 +27,7 @@ namespace Coding_Coalition_Project.Pages.AddAssignments
 
         [BindProperty]
         public Assignments Assignments { get; set; }
-
+        public string subType { get; set; }
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
@@ -36,7 +36,9 @@ namespace Coding_Coalition_Project.Pages.AddAssignments
             {
                 return Page();
             }
-
+            subType = Request.Form["subType"];
+            Assignments.submissionType = subType;
+            Assignments.AssignmentType = Request.Form["assignType"];
             Assignments.ClassID = (int)HttpContext.Session.GetInt32("CourseID");
             _context.Assignments.Add(Assignments);
             await _context.SaveChangesAsync();
